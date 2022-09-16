77ded36987ecf75f83687d92183743681ba0583d
IRONy <71219611+drevesina51@users.noreply.github.com>
2022-09-16 17:00:26 +0300

    Update grey_filter.c

--- lab5/grey_filter.c
+++ lab5/grey_filter.c
@@ -6,14 +6,19 @@
      for (unsigned char *p = img; p < img + size; p += 3)
      {
          unsigned char t = *p;
+         unsigned char t1 = *p;
          for (int i = 1; i < 3; ++i)
-         {
+         {      
              if (t > *(p + i))
              {
                  t = *(p + i);
              }
+             if (t1 < *(p + i))
+             {
+                 t1 = *(p + i);
+             }
          }
-         *pg = (int) (t);
+         *pg = ((int) (t) + (int) (t1)) / 2;
          ++pg;
      }
      return 0;
